cdbe3dec15d70fa1489feb1dcecd45bbc3727cd7
Pavel Uspensky <93841004+site2beru@users.noreply.github.com>
2022-09-24 21:38:01 +0300

    modal 24/09

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,20 +1,20 @@
 // Mobile menu
 
-// {
+{
 
-// let navMobile = document.querySelector('.mobile__list.closed'),
-//     btn = document.querySelector('.mobile__button');
+let navMobile = document.querySelector('.mobile__list.closed'),
+    btn = document.querySelector('.mobile__button');
 
-// btn.addEventListener('click', function() {
-//     if ( navMobile.classList.contains('closed') ) {  
-//     navMobile.classList.remove('closed');
-//     }
-//     else {
-//         navMobile.classList.add('closed');   
-//     }
-// });
+btn.addEventListener('click', function() {
+    if ( navMobile.classList.contains('closed') ) {  
+    navMobile.classList.remove('closed');
+    }
+    else {
+        navMobile.classList.add('closed');   
+    }
+});
 
-// }
+}
 
 // // Overlay div
 // {
